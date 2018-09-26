898e99fe8ae7c7d4db5d9ac4fff9292d895fb6bb
Smile <330855615@qq.com>
2018-09-26 11:33:53 +0800

    基本登录

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,7 @@ Route::get('user/{id}','UserController@show');
 //路由重定向 其中 here 表示原路由，there 表示重定向之后的路由，301 是一个 HTTP 状态码，用于标识重定向。
 Route::redirect('/here', '/there', 301);
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
